ad36c0b2282ff9208549b612e80cc27da36370ec
Inesa99 <inesamkrtchyan9999@gmail.com>
2023-08-07 18:38:11 +0400

    create DB

---
+++ Application/Services/ProductService.cs
@@ -0,0 +1,40 @@
+﻿using Application.Interfaces;
+using Common.ViewModels;
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class ProductService:IProduct
+    {
+        private readonly AppDbContext _context;
+
+        public ProductService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> Add(AddProductViewModel product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Update(EditProductViewModel product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductListViewModel> ProductList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(AddProductViewModel product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
